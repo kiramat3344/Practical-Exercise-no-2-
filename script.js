9a8e09d163649d237c8fa7662c34d78527f5b00c
kiramat3344 <kiramat0323@gmail.com>
2024-09-13 11:33:15 +0500

    this is new file

---
+++ script.js
@@ -0,0 +1,65 @@
+// Chacking Age Eligibility  (AND &&)
+
+// let age = +prompt("Enter Your Age:");
+
+// if (age >= 20 && age <= 45){
+//     alert("Your are Eligibile for this Job");
+// }
+// else{
+//     alert("Your are not Eligible for this Job");
+// }
+
+
+// Login Access (OR || )
+
+// let marks = +prompt("Enter Your Marks ");
+// let age = +prompt("Enter Your Age ");
+// if (marks >= 500 || age <= 18 ){
+//         alert("Your login is Successfully Thanks");
+//     }
+// else{
+//     alert("Your are not login");
+// }
+
+// Chacking Night Time (Not !)
+
+// let time = +prompt("Enter the Correct Time ");
+// if (time !=1 && time >12  ){
+//     if(time !=10){
+//         alert("This is night time ");
+//     }
+    
+//     }
+//     else{
+//         alert("this is day time");
+// }
+
+// Chacking odd and even Number
+
+// let Number = +prompt("Enter the number ")
+
+// if (Number%2 ==0){
+//     alert("Your numer is Even ");
+// }
+// else{
+//     alert("Your number is Odd");
+// }
+
+//  chacking weekend or weekday
+// function isWeekend(date) {
+//     // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+//     const dayOfWeek = date.getDay();
+    
+//     // Check if the day is Saturday (6) or Sunday (0)
+//     return dayOfWeek === 0 || dayOfWeek === 6;
+// }
+
+// // Example usage
+// const date1 = new Date('2024-09-14'); // A Saturday
+// const date2 = new Date('2024-09-16'); // A Monday
+
+// console.log(`Is ${date1.toDateString()} a weekend? ${isWeekend(date1)}`); // true
+// console.log(`Is ${date2.toDateString()} a weekend? ${isWeekend(date2)}`); // false
+
+
+
